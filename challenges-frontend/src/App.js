ca8a4b0eab2ef348186fc166ee5edb96d71f10da
tonim <lucas.rocha@dtidigital.com.br>
2021-08-24 21:05:36 -0300

    basic frontend for test purposes

---
+++ challenges-frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ChallengeComponent from './components/ChallengeComponent';
+
+function App() {
+  return (
+    <ChallengeComponent/>
+  );
+}
+
+export default App;
