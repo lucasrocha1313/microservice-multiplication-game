a86a4d8020baae3f9c3fd0da541567af3f815f89
tonim <lucas.rocha@dtidigital.com.br>
2021-09-25 17:23:45 -0300

    gamification: config load balancer

--- gamification/src/main/java/br/com/tonim/gamification/game/controllers/LeaderBoardController.java
+++ gamification/src/main/java/br/com/tonim/gamification/game/controllers/LeaderBoardController.java
@@ -2,6 +2,7 @@ package br.com.tonim.gamification.game.controllers;
 
 import br.com.tonim.gamification.game.domain.LeaderBoardRow;
 import br.com.tonim.gamification.game.services.LeaderBoardService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/leaders")
+@Slf4j
 public class LeaderBoardController {
     private final LeaderBoardService leaderBoardService;
 
     public LeaderBoardController(LeaderBoardService leaderBoardService) {
+        log.info("Retrieving leaderboard");
         this.leaderBoardService = leaderBoardService;
     }
 
