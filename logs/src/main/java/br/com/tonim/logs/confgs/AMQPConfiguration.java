0669453cd07b9c1b6fffe951c5d3d72b12d2026e
tonim <lucas.rocha@dtidigital.com.br>
2021-10-03 10:57:53 -0300

    config log capture

---
+++ logs/src/main/java/br/com/tonim/logs/confgs/AMQPConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.tonim.logs.confgs;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AMQPConfiguration {
+    @Bean
+    public TopicExchange logsExchange() {
+        return ExchangeBuilder.topicExchange("logs.topic")
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Queue logsQueue() {
+        return QueueBuilder.durable("logs.queue").build();
+    }
+
+    @Bean
+    public Binding logsBinding(final Queue logsQueue, final TopicExchange logsExhange) {
+        return BindingBuilder.bind(logsQueue).to(logsExhange).with("#");
+    }
+}
